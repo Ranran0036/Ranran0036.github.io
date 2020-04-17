1f61060c3ac859c361c42b61de24ef22a4838042
Ranran <ranran0036@163.com>
2020-04-18 00:33:51 +0800

    classSchedule 1.0

---
+++ js/classList.js
@@ -0,0 +1,69 @@
+let classList = [
+    {
+        name : '中国古建筑史论',
+        detial : [
+            {weeks : '3-10' , days : 1 , dayTime : [5,6] , classroom : '金16教314'},
+            {weeks : '3-10' , days : 2 , dayTime : [1,2] , classroom : '金16教314'}
+        ],
+        teacher : '严钧',
+        bgColor : ''
+    },
+    {
+        name : '建筑创作方法论',
+        detial : [
+            {weeks : '11-18' , days : 3 , dayTime : [1,3,4] , classroom : '金16教317'}
+        ],
+        teacher : '王蓉',
+        bgColor : ''
+    },
+    {
+        name : '注册建筑师、规划师理论与实践',
+        detial : [
+            {weeks : '3-10' , days : 3 , dayTime : [1,2] , classroom : '金16教314'},
+            {weeks : '3-10' , days : 4 , dayTime : [1,2] , classroom : '金16教314'}
+        ],
+        teacher : '许建和',
+        bgColor : ''
+    },
+    {
+        name : '建筑设计与实践',
+        detial : [
+            {weeks : '11-18' , days : 3 , dayTime : [5,6,7,8] , classroom : '金16教317'}
+        ],
+        teacher : '吕昀',
+        bgColor : ''
+    },
+    {
+        name : '绿色建筑与建筑节能',
+        detial : [
+            {weeks : '3-10' , days : 4 , dayTime : [3,4] , classroom : '金16教205'},
+            {weeks : '3-10' , days : 5 , dayTime : [5,6] , classroom : '金16教205'}
+        ],
+        teacher : '肖敏',
+        bgColor : ''
+    },
+    {
+        name : '城市设计理论与前沿',
+        detial : [
+            {weeks : '11-18' , days : 4 , dayTime : [5,6,7,8] , classroom : '金16教503'}
+        ],
+        teacher : '刘丹',
+        bgColor : ''
+    },
+    {
+        name : '自然辩证法概论',
+        detial : [
+            {weeks : '3-11' , days : 1 , dayTime : [3,4] , classroom : '金1A-104'}
+        ],
+        teacher : '刘佳男',
+        bgColor : ''
+    },
+    {
+        name : '工程硕士英语',
+        detial : [
+            {weeks : '3-18' , days : 5 , dayTime : [3,4] , classroom : '金4-109'}
+        ],
+        teacher : '马彦',
+        bgColor : ''
+    }
+]
